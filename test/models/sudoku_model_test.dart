77553f15dc0d92cdbf239dccdd2f2615f06f33f9
lachie james <lachie_james@hotmail.com.au>
2020-08-06 18:59:28 +1000

    Add tests for some of the sudoku

---
+++ test/models/sudoku_model_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sudoku_solver_2/constants/my_games.dart';
+import 'package:sudoku_solver_2/models/sudoku_model.dart';
+import 'package:sudoku_solver_2/models/tile_model.dart';
+
+void main() {
+  SudokuModel sudokuModel;
+
+  setUp(() {
+    sudokuModel = SudokuModel();
+  });
+
+  test('toString() is correctly formatted', () {
+    expect(sudokuModel.toString(), MyGames.emptySudokuString);
+  });
+
+  test('initTileMap - initialised with 81 tiles', () {
+    expect(sudokuModel.tileMap.length, 81);
+  });
+
+  test('initTileMap - tiles are all valid', () {
+    for (TileModel tileModel in sudokuModel.tileMap.values.toList()) {
+      expect(tileModel, isNotNull);
+      expect(1 <= tileModel.row && tileModel.row <= 9, true);
+      expect(1 <= tileModel.col && tileModel.col <= 9, true);
+      expect(tileModel.value, isNull);
+    }
+  });
+}
