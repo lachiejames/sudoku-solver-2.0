04b4b7e5442eb6b68eb0717bd29cf542c5922c4a
lachie james <lachie_james@hotmail.com.au>
2020-08-06 18:56:00 +1000

    Add some of the sudoku model

---
+++ lib/models/sudoku_model.dart
@@ -0,0 +1,43 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+import 'package:sudoku_solver_2/models/tile_key.dart';
+import 'package:sudoku_solver_2/models/tile_model.dart';
+
+class SudokuModel extends ChangeNotifier {
+  HashMap<TileKey, TileModel> tileMap;
+
+  SudokuModel() {
+    this.tileMap = this.initTileMap();
+  }
+
+  String toString() {
+    String s = '-------------------------------------\n';
+    for (int row = 1; row <= 9; row++) {
+      for (int col = 1; col <= 9; col++) {
+        if (this.getTileAt(row, col).value == null) {
+          s += '|   ';
+        } else {
+          s += '| ' + this.getTileAt(row, col).value.toString() + ' ';
+        }
+      }
+      s += '|\n-------------------------------------\n';
+    }
+
+    return s;
+  }
+
+  HashMap<TileKey, TileModel> initTileMap() {
+    HashMap<TileKey, TileModel> _tileMap = HashMap<TileKey, TileModel>();
+    for (int row = 1; row <= 9; row++) {
+      for (int col = 1; col <= 9; col++) {
+        _tileMap[TileKey(row: row, col: col)] = TileModel(null, row: row, col: col);
+      }
+    }
+    return _tileMap;
+  }
+
+  TileModel getTileAt(int row, int col) {
+    return tileMap[TileKey(row: row, col: col)];
+  }
+}
---
+++ lib/models/tile_key.dart
@@ -0,0 +1,13 @@
+class TileKey {
+  final int row;
+  final int col;
+
+  TileKey({this.row, this.col});
+
+  bool operator ==(o) => row == o.row && col == o.col;
+  int get hashCode => this.row * 11 + col * 89;
+
+  String toString() {
+    return 'TileKey($row, $col)';
+  }
+}
