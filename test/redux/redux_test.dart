8e758587246af7cbaaf093b76a2c3a9e4646c447
lachie james <lachie_james@hotmail.com.au>
2020-08-10 00:03:56 +1000

    Add tests for RemoveValueFromTileAction

--- test/redux/redux_test.dart
+++ test/redux/redux_test.dart
@@ -124,6 +124,19 @@ void main() {
         expect(numberState.isActive, true);
       }
     });
+
+    test('tapping another tile will deselect previous tile', () {
+      expect(state.tileStateMap[tileKey].isTapped, false);
+      dispatchActionAndUpdateState(TileSelectedAction(state.tileStateMap[tileKey]));
+      expect(state.tileStateMap[tileKey].isTapped, true);
+      expect(state.hasSelectedTile, true);
+
+      TileKey newTileKey = TileKey(row: 3, col: 4);
+      dispatchActionAndUpdateState(TileSelectedAction(state.tileStateMap[newTileKey]));
+      expect(state.tileStateMap[newTileKey].isTapped, true);
+      expect(state.tileStateMap[tileKey].isTapped, false);
+      expect(state.hasSelectedTile, true);
+    });
   });
 
   group('TileDeselectedAction & tileDeselectedReducer', () {
@@ -209,6 +222,97 @@ void main() {
     });
   });
 
+  group('RemoveValueFromTileAction & removeValueFromTileReducer', () {
+    TileKey tileKey = TileKey(row: 6, col: 9);
+
+    setUp(() {
+      // Need a tile to have a number first
+      dispatchActionAndUpdateState(TileSelectedAction(state.tileStateMap[tileKey]));
+      dispatchActionAndUpdateState(NumberPressedAction(state.numberStateList[3]));
+      dispatchActionAndUpdateState(TileSelectedAction(state.tileStateMap[tileKey]));
+    });
+
+    test('tileState replaced with new state', () {
+      TileState prevTileState = state.tileStateMap[tileKey];
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      TileState nextTileState = state.tileStateMap[tileKey];
+      expect(prevTileState == nextTileState, false);
+    });
+
+    test('tileStateMap replaced with new state', () {
+      HashMap<TileKey, TileState> prevTileStateMap = state.tileStateMap;
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      HashMap<TileKey, TileState> nextTileStateMap = state.tileStateMap;
+      expect(prevTileStateMap == nextTileStateMap, false);
+    });
+
+    test('numberStateList replaced with new state', () {
+      List<NumberState> prevNumberStateList = state.numberStateList;
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      List<NumberState> nextNumberStateList = state.numberStateList;
+      expect(prevNumberStateList == nextNumberStateList, false);
+    });
+
+    test('sets tile.isTapped to false', () {
+      expect(state.tileStateMap[tileKey].isTapped, true);
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      expect(state.tileStateMap[tileKey].isTapped, false);
+    });
+
+    test('sets state.selectedTile to null', () {
+      expect(state.hasSelectedTile, true);
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      expect(state.hasSelectedTile, false);
+    });
+
+    test('removes the tiles value', () {
+      expect(state.tileStateMap[tileKey].value, 4);
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+      expect(state.tileStateMap[tileKey].value, null);
+    });
+
+    test('tileStateMap is the same as the old tileStateMap, except for updated tileState', () {
+      HashMap<TileKey, TileState> prevTileStateMap = state.tileStateMap;
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      HashMap<TileKey, TileState> nextTileStateMap = state.tileStateMap;
+      for (int row = 1; row <= 9; row++) {
+        for (int col = 1; col <= 9; col++) {
+          if (row == tileKey.row && col == tileKey.col) {
+            expect(prevTileStateMap[TileKey(row: row, col: col)] == nextTileStateMap[TileKey(row: row, col: col)], false);
+          } else {
+            expect(prevTileStateMap[TileKey(row: row, col: col)] == nextTileStateMap[TileKey(row: row, col: col)], true);
+          }
+        }
+      }
+    });
+
+    test('all numberStates are now unactive', () {
+      List<NumberState> prevNumberStateList = state.numberStateList;
+      for (NumberState numberState in prevNumberStateList) {
+        expect(numberState.isActive, true);
+      }
+
+      dispatchActionAndUpdateState(RemoveValueFromTileAction(state.tileStateMap[tileKey]));
+
+      List<NumberState> nextNumberStateList = state.numberStateList;
+      for (NumberState numberState in nextNumberStateList) {
+        expect(numberState.isActive, false);
+      }
+    });
+  });
+
   group('NumberPressedAction & NumberPressedReducer', () {
     TileKey tileKey = TileKey(row: 6, col: 9);
     NumberState pressedNumber;
@@ -252,6 +356,18 @@ void main() {
       dispatchActionAndUpdateState(NumberPressedAction(pressedNumber));
 
       expect(state.tileStateMap[tileKey].value, 7);
+    });
+
+    test('sets tile.value to selected number, even if tile.value was not null before', () {
+      expect(state.tileStateMap[tileKey].value, null);
+
+      dispatchActionAndUpdateState(NumberPressedAction(pressedNumber));
+      expect(state.tileStateMap[tileKey].value, 7);
+
+      dispatchActionAndUpdateState(TileSelectedAction(state.tileStateMap[tileKey]));
+      pressedNumber = state.numberStateList[1];
+      dispatchActionAndUpdateState(NumberPressedAction(pressedNumber));
+      expect(state.tileStateMap[tileKey].value, 2);
     });
 
     test('sets tile.isTapped to false', () {
