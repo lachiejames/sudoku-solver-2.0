059c4fb659e616085734f3f0e3fb20a065ca385c
lachie james <lachie_james@hotmail.com.au>
2020-09-10 20:18:47 +1000

    Try running all tests and sonarqube

--- azure/pipelines/scripts.sh
+++ azure/pipelines/scripts.sh
@@ -4,6 +4,19 @@ set -e
 export PATH=$BUILD_SOURCESDIRECTORY/flutter/bin:$BUILD_SOURCESDIRECTORY/flutter/bin/cache/dart-sdk/bin:$PATH
 
 # All scripts will be placed here
+setup_emulator() {
+    echo "Installing Emulator SDK"
+    $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;default;x86'
+
+    echo "Creating Emulator"
+    $ANDROID_HOME/tools/bin/avdmanager create avd -n "pixel" --device "pixel" -k "system-images;android-29;default;x86"
+
+    echo "Starting Emulator"
+    $ANDROID_HOME/emulator/emulator -avd "pixel" -no-snapshot &
+    $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+    echo "Emulator started"
+}
+
 run_integration_tests() {
     echo "Running Integration Tests"
     flutter drive --target=test_driver/run_app.dart
