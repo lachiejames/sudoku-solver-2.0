bdb6d65a19f4ef44c2044570747ebe329ef8fd76
lachie james <lachie_james@hotmail.com.au>
2020-10-01 01:16:04 +1000

    Filled in some of the tests

--- test_driver/just_play_screen_tests.dart
+++ test_driver/just_play_screen_tests.dart
@@ -40,25 +40,48 @@ void main() {
         );
       });
 
-      test('tapping a selected tile changes color from green to white and removes X', () async {
-        for (int number = 1; number <= 9; number++) {
-          await expectNumberPropertiesToBe(
-            number: number,
-            color: 'white',
-          );
-        }
+      test('tapping a selected tile changes color from green to white', () async {
+        await tapTile(TileKey(row: 2, col: 2));
+
+        await expectTilePropertiesToBe(
+          tileKey: TileKey(row: 2, col: 2),
+          color: 'green',
+          textColor: 'black',
+          hasX: false,
+        );
 
         await tapTile(TileKey(row: 2, col: 2));
 
-        for (int number = 1; number <= 9; number++) {
-          await expectNumberPropertiesToBe(
-            number: number,
-            color: 'green',
-          );
-        }
+        await expectTilePropertiesToBe(
+          tileKey: TileKey(row: 2, col: 2),
+          color: 'white',
+          textColor: 'black',
+          hasX: false,
+        );
       });
 
       test('tapping a tile with a value adds an X', () async {
+        await addNumberToTile(7, TileKey(row: 2, col: 2));
+        await tapTile(TileKey(row: 2, col: 2));
+
+        await expectTilePropertiesToBe(
+          tileKey: TileKey(row: 2, col: 2),
+          color: 'green',
+          textColor: 'black',
+          hasX: true,
+        );
+
+        await tapTile(TileKey(row: 2, col: 2));
+
+        await expectTilePropertiesToBe(
+          tileKey: TileKey(row: 2, col: 2),
+          color: 'white',
+          textColor: 'black',
+          hasX: false,
+        );
+      });
+
+      test('tapping a tile changes all numbers from white to green', () async {
         for (int number = 1; number <= 9; number++) {
           await expectNumberPropertiesToBe(
             number: number,
@@ -75,11 +98,41 @@ void main() {
           );
         }
       });
+      
+      test('tapping a selected tile changes all numbers from green to white', () async {
+        await tapTile(TileKey(row: 2, col: 2));
 
-      test('tapping a tile changes all numbers from white to green', () async {});
-      test('tapping a selected tile changes all numbers from green to white', () async {});
+        for (int number = 1; number <= 9; number++) {
+          await expectNumberPropertiesToBe(
+            number: number,
+            color: 'green',
+          );
+        }
 
-      test('double tapping a tile removes its value', () async {});
+        await tapTile(TileKey(row: 2, col: 2));
+
+        for (int number = 1; number <= 9; number++) {
+          await expectNumberPropertiesToBe(
+            number: number,
+            color: 'white',
+          );
+        }
+      });
+
+      test('pressing a tile, then a number, should add that number to the tile', () async {
+        await expectNumberOnTileToBe(null, TileKey(row: 2, col: 2));
+
+        await addNumberToTile(7, TileKey(row: 2, col: 2));
+        await expectNumberOnTileToBe(7, TileKey(row: 2, col: 2));
+      });
+
+      test('double tapping a tile removes its value', () async {
+        await addNumberToTile(7, TileKey(row: 2, col: 2));
+        await expectNumberOnTileToBe(7, TileKey(row: 2, col: 2));
+
+        await doubleTapTile(TileKey(row: 2, col: 2));
+        await expectNumberOnTileToBe(null, TileKey(row: 2, col: 2));
+      });
 
       test('adding an invalid tile changes textColor from black to red', () async {});
       test('removing an invalid tile changes textColor from red to black', () async {});
@@ -91,7 +144,17 @@ void main() {
     });
 
     group('original tiles ->', () {
-      test('tapping a tile changes no colors, and does not remove the value', () async {});
+      test('tapping a tile changes no colors, and does not remove the value', () async {
+        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
+        await addNumberToTile(7, TileKey(row: 1, col: 1));
+        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
+      });
+
+      test('double tapping the tile should NOT remove its value', () async {
+        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
+        await doubleTapTile(TileKey(row: 1, col: 1));
+        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
+      });
     });
 
     group('restart ->', () {
@@ -112,66 +175,35 @@ void main() {
       test('pressing NEW GAME dehighlights all tiles and numbers', () async {});
     });
 
-    group('for regular tiles ->', () {
-      test('pressing a tile, then a number, should add that number to the tile', () async {
-        await expectNumberOnTileToBe(null, TileKey(row: 2, col: 2));
+    // group('playing a game ->', () {
+    //   test('initial values on the board should be the values in game0', () async {
+    //     await verifyInitialGameTiles(my_games.games[0]);
+    //   });
 
-        await addNumberToTile(7, TileKey(row: 2, col: 2));
-        await expectNumberOnTileToBe(7, TileKey(row: 2, col: 2));
-      });
+    //   test('adding correct values to all blank tiles will finish the game', () async {
+    //     await playGame(my_solved_games.solvedGamesList[0]);
+    //     await driver.getText(find.text('SOLVED'));
+    //     await driver.getText(find.text('NEW GAME'));
+    //   });
 
-      test('double tapping the tile should remove its value', () async {
-        await addNumberToTile(7, TileKey(row: 2, col: 2));
-        await expectNumberOnTileToBe(7, TileKey(row: 2, col: 2));
+    //   test('pressing NEW GAME will load a new sudoku', () async {
+    //     await driver.getText(find.text('Pick a tile'));
 
-        await doubleTapTile(TileKey(row: 2, col: 2));
-        await expectNumberOnTileToBe(null, TileKey(row: 2, col: 2));
-      });
-    });
+    //     await verifyInitialGameTiles(my_games.games[0]);
+    //     await playGame(my_solved_games.solvedGamesList[0]);
+    //     await tapNewGameButton();
 
-    group('for original tiles ->', () {
-      test('pressing a tile, then a number, should NOT add that number to the tile', () async {
-        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
-        await addNumberToTile(7, TileKey(row: 1, col: 1));
-        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
-      });
+    //     await driver.getText(find.text('Pick a tile'));
+    //     await verifyInitialGameTiles(my_games.games[1]);
+    //   });
 
-      test('double tapping the tile should NOT remove its value', () async {
-        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
-        await doubleTapTile(TileKey(row: 1, col: 1));
-        await expectNumberOnTileToBe(5, TileKey(row: 1, col: 1));
-      });
-    });
+    //   test('can play 2 games in a row', () async {
+    //     await playGame(my_solved_games.solvedGamesList[0]);
+    //     await tapNewGameButton();
 
-    group('playing a game ->', () {
-      test('initial values on the board should be the values in game0', () async {
-        await verifyInitialGameTiles(my_games.games[0]);
-      });
-
-      test('adding correct values to all blank tiles will finish the game', () async {
-        await playGame(my_solved_games.solvedGamesList[0]);
-        await driver.getText(find.text('SOLVED'));
-        await driver.getText(find.text('NEW GAME'));
-      });
-
-      test('pressing NEW GAME will load a new sudoku', () async {
-        await driver.getText(find.text('Pick a tile'));
-
-        await verifyInitialGameTiles(my_games.games[0]);
-        await playGame(my_solved_games.solvedGamesList[0]);
-        await tapNewGameButton();
-
-        await driver.getText(find.text('Pick a tile'));
-        await verifyInitialGameTiles(my_games.games[1]);
-      });
-
-      test('can play 2 games in a row', () async {
-        await playGame(my_solved_games.solvedGamesList[0]);
-        await tapNewGameButton();
-
-        await playGame(my_solved_games.solvedGamesList[1]);
-        await tapNewGameButton();
-      }, timeout: Timeout(Duration(seconds: 60)));
-    });
+    //     await playGame(my_solved_games.solvedGamesList[1]);
+    //     await tapNewGameButton();
+    //   }, timeout: Timeout(Duration(seconds: 60)));
+    // });
   });
 }
